5307170b5b3775306f08bcf6c370f1956539dc18
JPatel87 <jaime_patel@hotmail.co.uk>
2021-09-14 12:19:07 +0000

    Add while loop to introduction function to check for user name

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
 def introduction():
-    """Introductions and project purpose explained
+    """Introductions, while loop implemented to check for user name. 
+    Project purpose explained.
     """
     print("Welcome to Smoothie Selector!")
-    print("Lets start by introducing ourselves, my name is Fred")
-    name = input("What is your name? \n")
-    print(f"Hello {name}, nice to meet you! Hope your having a great day")
-    print("I am here to help you decide on what smooothie to make today, so without further ado lets make a start!")
-
+    print("Lets start by introducing ourselves, my name is Coco Nutt, Coco for short.")
+    while True:
+        name = input("What would you like to be called? \n")
+        if not name:
+            print("Sorry I didn't catch that...")
+            continue
+        if name:
+            print(f"Hello {name}, nice to meet you! Hope your having a great day")
+            print("I am here to help you decide on what smooothie to make today, so without further ado lets make a start!")
+            break
+            
 introduction()
