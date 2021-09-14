aa8b88ebf9b452458a8bb572fcc85a6b494f5689
JPatel87 <jaime_patel@hotmail.co.uk>
2021-09-14 10:32:01 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def introduction():
+    """Introductions and project purpose explained
+    """
+    print("Welcome to Smoothie Selector!")
+    print("Lets start by introducing ourselves, my name is Fred")
+    name = input("What is your name? \n")
+    print(f"Hello {name}, nice to meet you! Hope your having a great day")
+    print("I am here to help you decide on what smooothie to make today, so without further ado lets make a start!")
+
+introduction()
