a6ee645c3bfe9d4d7bbdaf67515c6cbf8e25b9b5
JPatel87 <jaime_patel@hotmail.co.uk>
2021-09-15 19:53:41 +0000

    Add smoothie ingrediants and smoothie details dictionaries into recipe.py file

--- recipes.py
+++ recipes.py
@@ -1,4 +1,31 @@
 smoothies = {
-    "Fruit": ["Berry and Plum", "Pomegranate and Strawberry", "Banana and Blueberry", "Strawberry and Watermeloom", "Mango and Banana"],
+    "Fruit": ["Peach and Rasberry", "Pomegranate and Strawberry", "Banana and Blueberry", "Strawberry and Watermelon", "Mango and Banana"],
     "Fruit_and_Veg": ["Carrot and Orange", "Cucumber and Watermelon", "Avacado and Banana", "Spinach and Banana", "Strawberry and Cauliflower"]
+}
+
+smoothie_ingrediants = {
+    "Peach and Rasberry": ["410g peach halves", "100g rasberries", "100ml orange juice", "150ml custard"], 
+    "Pomegranate and Strawberry": ["1 banana", "100g strawberries", "120mL pomegranate juice", "120g greek yoghurt", "30g honey"],
+    "Banana and Blueberry": ["150g blueberries", "1 banana", "120g vanilla yogurt", "120ml milk"],
+    "Strawberry and Watermelon": ["150g watermelon flesh", "150g watermelon rind", "150g strawberries", "15g Honey", "5ml lemon juice"],
+    "Mango and Banana": ["1 mango", "1 banana", "500ml orange juice"],
+    "Carrot and Orange": ["2 carrots", "2 oranges", "2cm piece of ginger, grated", "2 tbsp oats", "100g ice"]
+    "Cucumber and Watermelon": ["300g watermelon", "1/2 cucumber", "2 sprigs of mint", "honey to taste"]
+    "Avacado and Banana": ["1/2 avacado", "1 banana", "15g honey", "240ml cup milk"]
+    "Spinach and Banana": ["1 handful spinach", "1 banana", "240ml milk", "10g Nutella"]
+    "Strawberry and Cauliflower": ["1 cup cauliflower florets", "1 cup strawberries", "1 banana", "480ml milk"]
+
+}
+    
+smoothie_details = {
+    "Peach and Rasberry": {"Type": "F", "Calories/serving(kcal)": 161}
+    "Pomegranate and Strawberry": {"Type": "F", "Calories/serving(kcal)": 451}
+    "Banana and Blueberry": {"Type": "F", "Calories/serving(kcal)": 336}
+    "Strawberry and Watermelon": {"Type": "F", "Calories/serving(kcal)": 100}
+    "Mango and Banana": {"Type": "F", "Calories/serving(kcal)": 107}
+    "Carrot and Orange": {"Type": "FV", "Calories/serving(kcal)": 75}
+    "Cucumber and Watermelon": {"Type": "FV", "Calories/serving(kcal)": 62}
+    "Avacado and Banana": {"Type": "FV", "Calories/serving(kcal)": 224}
+    "Spinach and Banana": {"Type": "FV", "Calories/serving(kcal)": 286}
+    "Strawberry and Cauliflower": {"Type": "FV", "Calories/serving(kcal)": 149}
 }
